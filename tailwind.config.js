7248d1ac571c913f375d7c5293131205fdc13a70
Theonyxandco <theonyxandco@gmail.com>
2025-04-05 23:32:56 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#d4af37',
+      },
+    },
+  },
+  plugins: [],
+};
